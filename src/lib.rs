5d6b957c904c3582d4a916361cc6fefb7f276c22
Matija Pretnar <matija@pretnar.info>
2025-04-06 08:54:39 +0200

    Separate into lib & main

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+mod eval;
+mod state;
+mod syntax;
+
+pub use state::StackFrame;
+pub use syntax::Statement;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-mod eval;
-mod state;
-mod syntax;
-
-fn main() {}
+fn main() {
+    let mut frame = minirust::StackFrame::new();
+    let stmt = minirust::Statement::fibonacci(10);
+    let output = stmt.run(&mut frame);
+    for msg in output.printouts {
+        println!("{msg}");
+    }
+}
